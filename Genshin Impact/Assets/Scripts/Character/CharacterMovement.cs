018cb3bbe29ed8726fa42d90338db5d590df9b41
zeppoo <reinverhaag@gmail.com>
2023-11-22 11:21:42 +0100

    movement werkt niet perfect

--- Genshin Impact/Assets/Scripts/Character/CharacterMovement.cs
+++ Genshin Impact/Assets/Scripts/Character/CharacterMovement.cs
@@ -20,7 +20,7 @@ public class CharacterMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f) 
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg * cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
