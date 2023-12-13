929e3d3fe11bcafcaebd0eee50dd6d2fd84b404f
Davey <36044@ma-web.nl>
2023-12-13 10:27:17 +0100

    shooting gemaakt

--- Genshin_Impact/Assets/script/shoot.cs
+++ Genshin_Impact/Assets/script/shoot.cs
@@ -4,52 +4,62 @@ using UnityEngine;
 
 public class shoot : MonoBehaviour
 {
+    public float damageTime;
     public float damage;
     public GameObject prefab;
-    public Camera myCamera;
-
-    float x = Screen.width / 2;
-    float y = Screen.height / 2;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //var ray = myCamera.ScreenPointToRay(new Vector3(x, y, 0));
-        //clone.velocity = ray.direction * 80;
-    }
+    public Camera virtualCamera;
+    [SerializeField] public float shootCooldown = 1f;
+    public float shootTimer;
+    private bool hasFired;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
-        {
-            damage += 1 * Time.deltaTime;
-            
-            Debug.Log(damage.ToString());
+        shootTimer -= Time.deltaTime;
 
-            /*if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonDown(0) && shootTimer <= 0f)
+        {
+            damageTime = 0;
+            hasFired = false;
+        }
+        if (Input.GetMouseButton(0) && shootTimer <= 0f)
+        {
+
+            damageTime += 1 * Time.deltaTime;
+            Debug.Log(damageTime.ToString());
+        }
+        if (Input.GetMouseButtonUp(0) && shootTimer <= 0f)
+        {
+            if (damageTime >= 3f && !hasFired)
+            {
+                fullPowerFire();
+                hasFired = true;
+            }
+            else
             {
                 fire();
-                Debug.Log("release");
-                return;
-            }*/
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            fire();
+            }
+
             Debug.Log("release");
-            return;
+            shootTimer = shootCooldown;
         }
+    }
+
+    void fullPowerFire()
+    {
+        Debug.Log("full power");
+
+        fire();
     }
 
     void fire()
     {
         Debug.Log("je hebt geschoten");
         GameObject ob = Instantiate(prefab);
+        Vector3 shootDirection = virtualCamera.transform.forward;
         ob.transform.position = transform.position;
-        ob.transform.rotation = transform.rotation;
+        ob.transform.rotation = Quaternion.LookRotation(shootDirection); ;
         Destroy(ob, 3f);
     }
-    
+
 }
