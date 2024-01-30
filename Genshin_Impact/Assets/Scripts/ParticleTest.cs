ccbc6957dfbae623449bc221cf537c070f99901e
zeppoo <reinverhaag@gmail.com>
2024-01-30 21:12:44 +0100

    animations werken soort van

--- Genshin_Impact/Assets/Scripts/ParticleTest.cs
+++ Genshin_Impact/Assets/Scripts/ParticleTest.cs
@@ -10,14 +10,14 @@ public class ParticleTest : MonoBehaviour
         if (other.CompareTag("Arrow"))
         {
             GameObject newtext = Instantiate(damageText, other.transform.position, Quaternion.identity);
-            newtext.transform.position += new Vector3(Random.Range(1f, -1f), Random.Range(1f, -1f), Random.Range(1f, -1f));
             newtext.transform.LookAt(player);
+            newtext.transform.position += new Vector3(Random.Range(1f, -1f), Random.Range(1f, -1f), Random.Range(1f, -1f));
             newtext.GetComponent<Rigidbody>().AddForce(Vector3.up * 10);
             newtext.GetComponent<TextMesh>().characterSize = Random.Range(0.1f, 0.2f);
             newtext.GetComponent<TextMesh>().fontSize = 100;
             newtext.GetComponent<TextMesh>().text = Mathf.Round(other.GetComponent<Arrow>().dmg).ToString();
             Destroy(other.gameObject);
-            Destroy(newtext, 1f);
+            Destroy(newtext, 10f);
         }
     }
 }
