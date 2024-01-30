5bd98d533112c6e9afdf6847a73d348366e50e47
zeppoo <reinverhaag@gmail.com>
2024-01-30 11:51:28 +0100

    arrow particles

--- Genshin_Impact/Assets/Scripts/EnableCrossAir.cs
+++ Genshin_Impact/Assets/Scripts/EnableCrossAir.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HorribleScript : MonoBehaviour
+public class EnableCrossAir : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
     {
         GetComponent<Image>().color = CameraSwitch.shootingMode == true ? new Color(0f, 0f, 0f, 100f) : new Color(0f, 0f, 0f, 0f);
-    }    
+    }
 }
